dd0770bfa2c90d5b20605e102117bf182a6a12f5
Konstantin S. Belyaev <ks.belyaev@gmail.com>
2014-10-31 11:15:23 +0300

    r10k support

--- provision/puppet/modules/profiles/manifests/puppet-server.pp
+++ provision/puppet/modules/profiles/manifests/puppet-server.pp
@@ -1,4 +1,17 @@
 # vim: ts=4 sts=4 sw=4 expandtab
 class profiles::puppet-server {
     include puppet
+
+    package { 'r10k':
+        provider => 'gem',
+        require => Class['puppet']
+    }
+
+    file { '/etc/r10k.yaml':
+        ensure  => 'present',
+        owner   => 'root',
+        group   => 'root',
+        source  => 'puppet:///modules/profiles/r10k.yaml',
+        require => Package['r10k'],
+    }
 }
